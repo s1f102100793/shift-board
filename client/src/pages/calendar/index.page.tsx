4611d255ba51dead184da0bd169d25d15a7f415c
s1f102100793 <s1f102100793@iniad.org>
2023-08-20 14:09:39 +0900

    notchore

---
+++ client/src/pages/calendar/index.page.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import styles from './ShiftBoard.module.css';
+
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+function getDaysInMonth(month: number, year: number) {
+  return new Date(year, month + 1, 0).getDate();
+}
+
+function getFirstDayOfMonth(month: number, year: number) {
+  return new Date(year, month, 1).getDay();
+}
+
+const ShiftBoard: React.FC = () => {
+  const now = new Date();
+  const [selectedMonth, setSelectedMonth] = useState(now.getMonth());
+  const [selectedYear, setSelectedYear] = useState(now.getFullYear());
+  const [calendarData, setCalendarData] = useState<(number | null)[]>([]);
+
+  useEffect(() => {
+    const daysInMonth = getDaysInMonth(selectedMonth, selectedYear);
+    const firstDayOfMonth = getFirstDayOfMonth(selectedMonth, selectedYear);
+
+    const data = [];
+
+    for (let i = 0; i < firstDayOfMonth; i++) {
+      data.push(null);
+    }
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      data.push(i);
+    }
+
+    setCalendarData(data);
+  }, [selectedMonth, selectedYear]);
+
+  const navigateMonth = (offset: number) => {
+    let newMonth = selectedMonth + offset;
+    let newYear = selectedYear;
+
+    if (newMonth > 11) {
+      newMonth = 0;
+      newYear++;
+    } else if (newMonth < 0) {
+      newMonth = 11;
+      newYear--;
+    }
+
+    setSelectedMonth(newMonth);
+    setSelectedYear(newYear);
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* Upper Half: Calendar */}
+      <div className={styles.calendarSection}>
+        <div className={styles.monthNavigation}>
+          <button className={styles.navButton} onClick={() => navigateMonth(-1)}>
+            ＜
+          </button>
+          <span>
+            {MONTHS[selectedMonth]} {selectedYear}
+          </span>
+          <button className={styles.navButton} onClick={() => navigateMonth(1)}>
+            ＞
+          </button>
+        </div>
+        <div className={styles.calendarGrid}>
+          {calendarData.map((day, index) => (
+            <div key={index} className={styles.calendarDay}>
+              {day}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Lower Half: Shift Input */}
+      <div className={styles.shiftInputSection}>{/* ... (the rest remains unchanged) */}</div>
+    </div>
+  );
+};
+
+export default ShiftBoard;
