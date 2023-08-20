0f892ea0c59dd9c6d3f395151c56b6bd3f0a4f39
s1f102100793 <s1f102100793@iniad.org>
2023-08-20 20:55:32 +0900

    ラティさん最強

--- client/src/pages/calendar/index.page.tsx
+++ client/src/pages/calendar/index.page.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { ActionIcon } from '@mantine/core';
+import { TimeInput } from '@mantine/dates';
+import { IconClock } from '@tabler/icons-react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './ShiftBoard.module.css';
 
 const MONTHS = [
@@ -36,6 +39,7 @@ const ShiftBoard: React.FC = () => {
   const [holidays, setHolidays] = useState<{ [date: string]: string }>({});
   const [showShiftBar, setShowShiftBar] = useState(false);
   const [selectedDays, setSelectedDays] = useState<number[]>([]);
+  const ref = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const daysInMonth = getDaysInMonth(selectedMonth, selectedYear);
@@ -86,6 +90,15 @@ const ShiftBoard: React.FC = () => {
     setSelectedDays((prevDays) =>
       prevDays.includes(day) ? prevDays.filter((d) => d !== day) : [...prevDays, day]
     );
+  };
+
+  const timeSlots = Array.from(new Array(24 * 2)).map(
+    (_, index) =>
+      `${index < 20 ? '0' : ''}${Math.floor(index / 2)}:${index % 2 === 0 ? '00' : '30'}`
+  );
+
+  const clearSelectedDays = () => {
+    setSelectedDays([]);
   };
 
   return (
@@ -144,6 +157,11 @@ const ShiftBoard: React.FC = () => {
         </div>
       </div>
       <div className={styles.shiftInputSection}>
+        <div className={styles.clearButtonContainer}>
+          <button className={styles.clearButton} onClick={clearSelectedDays}>
+            選択をクリア
+          </button>
+        </div>
         <div className={styles.selectedDaysSection}>
           選択された日： {selectedDays.map((day) => `${MONTHS[selectedMonth]} ${day}`).join(', ')}
         </div>
@@ -152,9 +170,51 @@ const ShiftBoard: React.FC = () => {
         </button>
         <div className={`${styles.shiftBar} ${showShiftBar ? styles.shiftBarVisible : ''}`}>
           <button onClick={() => setShowShiftBar(false)}>閉じる</button>
+          {/* <Autocomplete
+            id="disabled-options-demo"
+            options={timeSlots}
+            getOptionDisabled={(option) => option === timeSlots[0] || option === timeSlots[2]}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="バイト開始時間" />}
+          /> */}
+          <TimeInput
+            label="Click icon to show browser picker"
+            ref={ref}
+            step={1800} // 30分おきの選択
+            rightSection={
+              <ActionIcon onClick={() => ref.current && ref.current.showPicker()}>
+                <IconClock size="1rem" stroke={1.5} />
+              </ActionIcon>
+            }
+            maw={400}
+            mx="auto"
+          />
+
+          <label>
+            開始時間:
+            <input type="time" id="start-time" />
+          </label>
+          <label>
+            終了時間:
+            <input type="time" id="end-time" />
+          </label>
+
           <button
             onClick={() => {
-              /* シフトの詳細入力処理 */
+              const startTimeElement = document.getElementById('start-time') as HTMLInputElement;
+              const endTimeElement = document.getElementById('end-time') as HTMLInputElement;
+
+              const startTime = startTimeElement !== null ? startTimeElement.value : '';
+              const endTime = endTimeElement !== null ? endTimeElement.value : '';
+
+              if (startTime && endTime) {
+                // ここでシフトの時間設定に関連する処理を行います。
+                console.log(`シフト開始時間: ${startTime}, シフト終了時間: ${endTime}`);
+
+                // 例: APIにシフト時間を送信する、シフト時間を画面に表示するなど
+              } else {
+                alert('開始時間と終了時間の両方を入力してください。');
+              }
             }}
           >
             シフトを追加する
