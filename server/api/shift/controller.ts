82b4c0cbe7940b91679221aa13393a7821b59c80
s1f102100793 <s1f102100793@iniad.org>
2023-08-21 23:10:12 +0900

    createがうまくいかない

--- server/api/shift/controller.ts
+++ server/api/shift/controller.ts
@@ -1,10 +1,18 @@
-import { shiftRepository } from '$/repository/shiftRepository';
+import { createShift } from '$/repository/shiftRepository';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
   get: () => ({ status: 200, body: 'Hello' }),
+  // post: async ({ body }) => ({
+  //   status: 201,
+  //   body: await createOrUpdateShift(body.id, body.date, body.starttime, body.endtime),
+  // }),
+  // post: async ({ body }) => ({
+  //   status: 201,
+  //   body: await shiftRepository.save(body.id, body.date, body.starttime, body.endtime),
+  // }),
   post: async ({ body }) => ({
     status: 201,
-    body: await shiftRepository.save(body.id, body.date, body.starttime, body.endtime),
+    body: await createShift(body.id, body.date, body.starttime, body.endtime),
   }),
 }));
--- server/repository/shiftRepository.ts
+++ server/repository/shiftRepository.ts
@@ -10,35 +10,48 @@ export const toShiftModel = (prismaShift: Shift): ShiftModel => ({
   endtime: prismaShift.endtime,
 });
 
-// export const createShift = async (
+export const createShift = async (
+  id: ShiftModel['id'],
+  date: ShiftModel['date'],
+  starttime: ShiftModel['starttime'],
+  endtime: ShiftModel['endtime']
+): Promise<ShiftModel> => {
+  console.log('来ている');
+  const prismaShift = await prismaClient.shift.create({
+    data: { id, date, starttime, endtime },
+  });
+  return toShiftModel(prismaShift);
+};
+// export const createOrUpdateShift = async (
 //   id: ShiftModel['id'],
 //   date: ShiftModel['date'],
 //   starttime: ShiftModel['starttime'],
 //   endtime: ShiftModel['endtime']
 // ): Promise<ShiftModel> => {
-//   console.log('来ている');
-//   const prismaShift = await prismaClient.shift.create({
-//     data: { id, date, starttime, endtime },
+//   const prismaShift = await prismaClient.shift.upsert({
+//     where: { id_date: { id, date } },
+//     create: { id, date, starttime, endtime },
+//     update: { starttime, endtime },
 //   });
 //   return toShiftModel(prismaShift);
 // };
 
-export const shiftRepository = {
-  save: async (id: string, date: string, starttime: string, endtime: string) => {
-    await prismaClient.shift.upsert({
-      where: { id, date }, // 一意な条件を指定
-      create: {
-        id,
-        date,
-        starttime,
-        endtime,
-      },
-      update: {
-        date,
-        starttime,
-        endtime,
-      },
-    });
-    return;
-  },
-};
+// export const shiftRepository = {
+//   save: async (id: string, date: string, starttime: string, endtime: string) => {
+//     await prismaClient.shift.upsert({
+//       where: { id, date }, // 一意な条件を指定
+//       create: {
+//         id,
+//         date,
+//         starttime,
+//         endtime,
+//       },
+//       update: {
+//         date,
+//         starttime,
+//         endtime,
+//       },
+//     });
+//     return;
+//   },
+// };
