59930481aded9c30a806861a5d76a9172fff756f
s1f102100793 <s1f102100793@iniad.org>
2023-08-24 22:02:01 +0900

    見た目を変更

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -5,13 +5,27 @@ import styles from './index.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>シフトボード</h1>
-      <Link href={pagesPath.fixedcalendar.$url()} legacyBehavior>
-        <button className={styles.button}>カレンダーを見る</button>
-      </Link>
-      <Link href={pagesPath.calendar.$url()} legacyBehavior>
-        <button className={styles.button}>シフトを提出する</button>
-      </Link>
+      <header className={styles.header}>
+        <h1 className={styles.logo}>シフトボード</h1>
+      </header>
+      <main className={styles.mainContent}>
+        <section className={styles.intro}>
+          <p>
+            シフト管理がこれまで以上に簡単に。シフトボードであなたのスケジュールを効率的に管理しましょう。
+          </p>
+        </section>
+        <section className={styles.actions}>
+          <Link href={pagesPath.fixedcalendar.$url()} legacyBehavior>
+            <a className={styles.button}>カレンダーを見る</a>
+          </Link>
+          <Link href={pagesPath.calendar.$url()} legacyBehavior>
+            <a className={styles.button}>シフトを提出する</a>
+          </Link>
+        </section>
+      </main>
+      <footer className={styles.footer}>
+        <p>&copy; 2023 シフトボード. すべての権利を保持。</p>
+      </footer>
     </div>
   );
 };
